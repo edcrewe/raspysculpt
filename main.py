dec74b3a03fd0c1d892715e982b50fade5f0e5d1
Ed Crewe <ed.crewe@enterprisedb.com>
2024-01-14 16:50:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
 # modules
-from picozero import Servo  # importing Servo class to easily control the servo motor
+ # importing Servo class to easily control the servo motor - picozero same as gpiozero
+from picozero import Servo 
 from time import sleep
 
 # creating a Servo object
 servos = [Servo(1), Servo(2), Servo(3), Servo(4), Servo(5), Servo(6)]
-angles = [0, 0.2, 0.4, 0.6, 0.8, 1]
-clockwise = [True, True, True, True, True, True]
-step = 0.05
+angles = [0, 1, 0, 1, 0, 1]
 
-# continuously swing the servo arm to min, mid, and max positions (for a duration of 1 sec each)
-while True:
-    # swinging the servo arm to its min position
+
+for x in range(0, 20):
+    plusminus = [1, -1, 1, -1, 1, -1]
     for i, servo in enumerate(servos):
         servo.value = angles[i]
 
     # swinging the servo arm to its mid position
     for i, servo in enumerate(servos):
-        if clockwise[i]:
-            angles[i] = angles[i] + 0.05
-        else:
-            angles[i] = angles[i] - 0.05           
-        if angles[i] > 1.5 or angles[i] < -0.5:
-            clockwise[i] = not clockwise[i]
+        angles[i] = angles[i] + (0.2 * plusminus[i])
+        if angles[i] > 1:
+            angles[i] = 0
+            plusminus[i] = -1
+        if angles[i] < 0:
+            angles[i] = 1
+            plusminus[i] = 1
+        servo.value = angles[i]
+
+    sleep(0.5)
+
+
+for x in range(0, 3):
+    for x in range(0, 5):
+        servos[x].min()
+        servos[x + 1].max()
+        sleep(1)
+        servos[x].mid()
+        servos[x + 1].mid()
+        sleep(1)
+        servos[x].max()
+        servos[x + 1].min()
+
+
+for x in range(0, 20):
+    angles = [0, 0.2, 0.4, 0.6, 0.8, 1]
+
+    # swinging the servo arm to its min position
+    for i, servo in enumerate(servos):
+        servo.value = angles[i]
+
+    # ticking the servos around at same rate
+    for i, servo in enumerate(servos):
+        angles[i] = angles[i] + 0.05
+        if angles[i] > 1:
+            angles[i] = 0
         servo.value = angles[i]
 
     sleep(0.3)
